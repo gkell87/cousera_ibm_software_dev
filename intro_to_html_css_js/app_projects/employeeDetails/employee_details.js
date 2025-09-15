9ea43d195bf417fc5ffd159bb7452aad129cc942
gkell87 <g_kell87@yahoo.com>
2025-09-15 11:16:55 -0400

    added specialization

--- intro_to_html_css_js/app_projects/employeeDetails/employee_details.js
+++ intro_to_html_css_js/app_projects/employeeDetails/employee_details.js
@@ -1,8 +1,8 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-    { id: 3, name: 'Burt Gordon', age: 32, department: 'Custodial', salary: 40000 }
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization : 'Javascript'},
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization : 'Javasc'},
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization : 'Javascript'},
+    { id: 3, name: 'Burt Gordon', age: 32, department: 'Custodial', salary: 40000, specialization : 'Javascript' }
     //... More employee records can be added here
   ];
 
@@ -44,6 +44,6 @@ function findEmployeeById(employeeId) {
     document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     }
     else{
-      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this Specialization';
+      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ';
      }
  }
