aa7abf2852150d0effb224916c4ef9fb5e720c24
gkell87 <g_kell87@yahoo.com>
2025-09-15 11:19:23 -0400

    fixed employees

--- intro_to_html_css_js/app_projects/employeeDetails/employee_details.js
+++ intro_to_html_css_js/app_projects/employeeDetails/employee_details.js
@@ -1,8 +1,8 @@
 const employees = [
     { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization : 'Javascript'},
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization : 'Javasc'},
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization : 'Javascript'},
-    { id: 3, name: 'Burt Gordon', age: 32, department: 'Custodial', salary: 40000, specialization : 'Javascript' }
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization : 'Python'},
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization : 'Java'},
+    { id: 3, name: 'Burt Gordon', age: 32, department: 'Custodial', salary: 40000, specialization : 'C++'}
     //... More employee records can be added here
   ];
 
