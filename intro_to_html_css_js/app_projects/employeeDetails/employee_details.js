c5d0600eb0f57a86808a1e52a0f5c96c2d204240
gkell87 <g_kell87@yahoo.com>
2025-09-15 10:38:16 -0400

    added js file with functions

---
+++ intro_to_html_css_js/app_projects/employeeDetails/employee_details.js
@@ -0,0 +1,40 @@
+const employees = [
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 3, name: 'Burt Gordon', age: 32, department: 'Custodial', salary: 40000 }
+    //... More employee records can be added here
+  ];
+
+  // Create function to display empolyees
+  function displayEmployees() {
+    const totalEmployees = employees
+        .map(employee => '<p>${employee.id} : ${employee.name} - ${employee.department} - $${employee.salary}</p>')
+        .join('');
+    document.getElementById('employeeDetails').innerHTML = totalEmployees;
+  }
+
+// Create function for total salaries
+// The reduce method iterates through each employee and accumulates their salaries.
+function calculateTotalSalaries() {
+    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert('Total Salaries: $${totalSalaries}');
+}
+
+function displayHREmployees() {
+    consthrEmployees = employees.filter(employee => employee.department === 'HR');
+     const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+     document.getElementById('employeeDetails').innerHTML = hrEmployeesDisplay;
+}
+
+
+
+function findEmployeeByID(employeeId) {
+    const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if (foundEmployee) {
+        document.getElementsById('employeeDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`
+    }
+    else{
+        document.getElementById(employeesDetail).innerHTML = 'no employees found with this ID'
+    }
+}
